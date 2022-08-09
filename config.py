ac9463f016e5febc31ec71c9a7d0e34b3b77b171
skjr7 <suryaskv003@gmail.com>
2022-08-10 01:12:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID", "19197570"))
-API_HASH = getenv("API_HASH", "0401e837796d193ec9ae6e17cb8cfbf8")
-BOT_TOKEN = getenv("BOT_TOKEN", "5533302610:AAFuXxCFlgs7bTUSEm_N2zmKXevwNcyPsSc")
+API_ID = int(getenv("API_ID", "13399054"))
+API_HASH = getenv("API_HASH", "585801d590dac4c79aeaa7bcda495e62")
+BOT_TOKEN = getenv("BOT_TOKEN", "5498862653:AAGjcn8ynlmnwa639gV0i3pLRnAH9pQB0YE")
 BOT_NAME = getenv("BOT_NAME","one love")
 BOT_USERNAME = getenv("BOT_USERNAME", "Tj_Onelove_Music_bot")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "king_of_izzy")
@@ -18,4 +18,4 @@ PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/89cbc8b8760b6abff430f.jpg
 SESSION_NAME = getenv("SESSION_NAME", "BQB_aMgcPgk7moBijoErjMZPOQvcDvB6xxdX4w6au4yGmkoGGhrgrpxOymbeuezkLRC6wr-wYVkmU-HXntg2_25HKZ_oXGhky5Tv2Qtg1I6cOJX8W3NG4RQJaQZBMhj9MwWCblZMkWzLEocmeeKsgX1dZ6sgh5aj-lCG5MZMvUE0bbYEHHvzFYYP0jx5d7Pxh8DE342n7i0UJOHQor7T8z6RbgDlzCWuSiTshi_xuzjyR_HptcJWdupOmR6Ael5mj-iHHA4ojQ1UmU65O8sRp-zOFbrd2L-YSKzf3S0ZxUBXh0xPhIFKCZZtUw87v41ZVna4ds6xGe-GUaCGf6z_dlNkAAAAAUcdewgA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "? ~ + • / ! ^ .").split())
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5429873872").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5457506177").split()))
